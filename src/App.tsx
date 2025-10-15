93227659ec1d77935b3408b8541d97756cbee558
m-ejiri <mizuho-ejiri@telema.jp>
2025-10-15 14:10:56 +0900

    installed Amplify

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+import { useAuthenticator } from "@aws-amplify/ui-react";
 import AppLayout from "./layouts/AppLayout";
 import Home from "./pages/Home";
 import Auth from "./pages/Auth";
@@ -6,10 +7,42 @@ import Unicorns from "./pages/Unicorns";
 import Investors from "./pages/Investors";
 import Faq from "./pages/Faq";
 import Apply from "./pages/Apply";
+import { useAuth } from "react-oidc-context";
 
 export default function App() {
+  const auth = useAuth();
+
+  const signOutRedirect = () => {
+    const clientId = "5q1uu3auhi0bia4r2qqq4744rk";
+    const logoutUri = "<logout uri>";
+    const cognitoDomain = "https://<user pool domain>";
+    window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
+  };
+
+  if (auth.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (auth.error) {
+    return <div>Encountering error... {auth.error.message}</div>;
+  }
+
+  if (auth.isAuthenticated) {
+    return (
+      <div>
+        <pre> Hello: {auth.user?.profile.email} </pre>
+        <pre> ID Token: {auth.user?.id_token} </pre>
+        <pre> Access Token: {auth.user?.access_token} </pre>
+        <pre> Refresh Token: {auth.user?.refresh_token} </pre>
+
+        <button onClick={() => auth.removeUser()}>Sign out</button>
+      </div>
+    );
+  }
   return (
     <AppLayout>
+      <button onClick={() => auth.signinRedirect()}>Sign in</button>
+      <button onClick={() => signOutRedirect()}>Sign out</button>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/auth" element={<Auth />} />
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Authenticator } from "@aws-amplify/ui-react";
+import { AuthProvider } from "react-oidc-context";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import "@aws-amplify/ui-react/styles.css";
+
+const cognitoAuthConfig = {
+  authority:
+    "https://cognito-idp.ap-northeast-1.amazonaws.com/ap-northeast-1_gYybLrBns",
+  client_id: "5q1uu3auhi0bia4r2qqq4744rk",
+  redirect_uri: "https://https://main.d21afdbd9zqj5m.amplifyapp.com/",
+  response_type: "code",
+  scope: "phone openid email",
+};
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Authenticator>
+      <AuthProvider {...cognitoAuthConfig}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </AuthProvider>
+    </Authenticator>
   </React.StrictMode>
 );
